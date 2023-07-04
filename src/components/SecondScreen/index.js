87d0fcde2217599ab1cc40e9f9905bd359d9f772
Anastasya Opredelentseva <nastyaop@mail.ru>
2023-07-04 23:34:45 +0300

    Time

--- src/components/SecondScreen/index.js
+++ src/components/SecondScreen/index.js
@@ -1,5 +1,88 @@
-import { Container } from "./styles";
+import {
+  Container,
+  Line,
+  Text,
+  Texts,
+  Pictures,
+  SwiperCont,
+  Cal,
+} from "./styles";
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+import p1 from "./assets/b1.jpeg";
+import p2 from "./assets/b2.jpeg";
+import p3 from "./assets/b3.jpeg";
+import p4 from "./assets/b4.jpeg";
+import p5 from "./assets/b5.jpeg";
+import p6 from "./assets/b6.jpeg";
+import p7 from "./assets/b7.jpeg";
+import b from "./assets/b.jpeg";
 
 export default function SecondScreen() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Line />
+      <Text>Дорогие друзья!</Text>
+      <Texts>
+        Для нас один день в этом году будет особенным и мы хотим провести его в
+        кругу близких и друзей. С большим удовольствием приглашаем вас на
+        знаменательный праздник - нашу свадьбу!
+      </Texts>
+      <Texts>Ждем ВАС</Texts>
+      <Text>18 августа 2023</Text>
+      <Texts>
+        Эко-комплекс Сурские берега (РМ, Кочкуровский район, село Мордовское
+        Давыдово)
+      </Texts>
+      <Line />
+      <SwiperCont
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+      >
+        <SwiperSlide>
+          <Pictures src={p1} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Pictures src={p2} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Pictures src={p3} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Pictures src={p4} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Pictures src={p5} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Pictures src={p6} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Pictures src={p7} />
+        </SwiperSlide>
+      </SwiperCont>
+      <Line />
+      <Text>Как добраться</Text>
+      <Texts>
+        Для вашего удобства мы подготовили карту. Надеемся, что вы легко найдете
+        место проведения свадьбы и порадуете нас своим присутствием!
+      </Texts>
+      <Pictures src={b} />
+      <Line />
+      <Text>Август</Text>
+      <Cal></Cal>
+      <Line />
+    </Container>
+  );
 }
--- src/components/SecondScreen/styles.js
+++ src/components/SecondScreen/styles.js
@@ -1,5 +1,61 @@
 import styled from "styled-components";
+import { Swiper } from "swiper/react";
+import calen from "./assets/calen.jpg";
+
+import heart from "./assets/heart.jpg";
 
 export const Container = styled.div`
-  padding: 80px 20px;
+  padding: 0 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Line = styled.div`
+  margin: 80px 0;
+  width: 80px;
+  border-bottom: 1px solid black;
+`;
+
+export const Text = styled.span`
+  font-family: "Allegretto";
+  font-size: 42px;
+  margin-bottom: 32px;
+`;
+
+export const Texts = styled.span`
+  font-family: "Mgnon";
+  font-size: 20px;
+  text-align: center;
+  margin-bottom: 18px;
+`;
+
+export const Pictures = styled.img`
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+  border-radius: 10px;
+  max-width: calc(100vw - 40px);
+
+  @media (max-width: 690px) {
+    height: 420px;
+  }
+`;
+
+export const Cal = styled.div`
+  width: 350px;
+  height: 280px;
+  border-radius: 10px;
+  max-width: calc(100vw - 40px);
+  position: relative;
+  background-image: url(${calen});
+  background-size: 350px 280px;
+`;
+export const Hearts = styled.div`
+  background-image: url(${heart});
+  background-size: 350px 280px;
+`;
+export const SwiperCont = styled(Swiper)`
+  max-width: calc(100vw - 40px);
+  --swiper-theme-color: white;
 `;
