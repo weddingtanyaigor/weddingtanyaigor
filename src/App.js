72ac115cbf6118dbcbf6c2cdd88ca2caef3bf70c
Anastasya Opredelentseva <nastyaop@mail.ru>
2023-07-06 00:06:54 +0300

    Changes for texts

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <>
       <FirstScreen />
       <SecondScreen />
+      <FirstScreen last />
     </>
   );
 }
--- src/components/FirstScreen/index.js
+++ src/components/FirstScreen/index.js
@@ -1,12 +1,12 @@
 import { Container, Names, Day, DayDate } from "./styles";
 
-export default function BannerComponent() {
+export default function BannerComponent(last) {
   return (
     <>
       <Container>
         <DayDate>18.08.2023</DayDate>
         <Day>Wedding Day</Day>
-        <Names>Igor & Tatiana</Names>
+        {!last && <Names>Igor & Tatiana</Names>}
       </Container>
     </>
   );
