d787f2a2b75486f826dee3a3811efc7fff949d91
Anastasya Opredelentseva <nastyaop@mail.ru>
2023-07-06 00:20:19 +0300

    Back names

--- src/components/FirstScreen/index.js
+++ src/components/FirstScreen/index.js
@@ -6,8 +6,9 @@ export default function BannerComponent(last) {
       <Container>
         <DayDate>18.08.2023</DayDate>
         <Day>Wedding Day</Day>
-        {!last && <Names>Igor & Tatiana</Names>}
+        <Names>Igor & Tatiana</Names>
       </Container>
+      чц
     </>
   );
 }
